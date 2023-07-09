3be8d321eb1366e542897f37782f7f2759e18f9e
Alina Yagubova <littlefoool@mail.ru>
2023-07-09 23:26:52 +0300

    temporary save for tree view

---
+++ TotallyWorkingBlazorShowcase/Pages/TreeView.razor.cs
@@ -0,0 +1,49 @@
+﻿// TreeView.razor.cs
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace TotallyWorkingBlazorShowcase.Pages
+{
+    public class TreeNode {
+        public string Name { get; set; }
+        public List<TreeNode> Childrens { get; set; }
+        public bool IsExpanded { get; set; } 
+    }
+
+    public class TreeViewModel: ComponentBase
+    {
+        // Модель данных дерева
+        public TreeNode treeData = new()
+        {
+            Name = "User 1",
+            Childrens = new List<TreeNode> { new() { Name = "User 2", Childrens = new List<TreeNode>() }, new() {Name = "USer 3", Childrens = new List<TreeNode>()} }
+        };
+        //{
+            /*new TreeNode
+            {
+                Name = "User 1",
+                Children = new List<TreeNode>
+                {
+                    new() { Name = "Computer 1" },
+                    new() { Name = "Computer 2" }
+                }
+            },
+            new TreeNode
+            {
+                Name = "User 2",
+                Children = new List<TreeNode>
+                {
+                    new()
+                    {
+                        Name = "Nested User",
+                        Children = new List<TreeNode>
+                        {
+                            new() { Name = "Nested Computer 1" },
+                            new() { Name = "Nested Computer 2" }
+                        }
+                    }
+                }
+            }*/
+        };
+}
